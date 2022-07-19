7548588d7b2a03e0b8c0724ab4fc8e35394ac3b5
Sydney <sdny7293@gmail.com>
2022-07-19 00:08:10 -0700

    Basic layout of tabs with links (no direction yet)

---
+++ NetworkMiniTab/networkMiniTab.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta name="description" contents="chatbox main screen" />
+    <title>Network minitab</title>
+    <link rel="stylesheet" href="css/chatbox.css" />
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- Font -->
+    <link rel="stylesheet" href="/COSC310G1/chatMessages/css/theme.css" />
+</head>
+
+<body>
+    <div class="m-auto container w-25 rounded-2 border border-secondary">
+        <div class="col py-2 px-3">
+            <div class="row">
+                Manage my network
+            </div>
+            <div class = "text-muted">
+                <div class="row"> 
+                    <a href = "#" class = "text-decoration-none py-1">Connections</a>
+                </div>
+                <div class="row">
+                    <a href = "#" class = "text-decoration-none py-1">Contacts</a>
+                </div>
+                <div class="row">
+                    <a href = "#" class = "text-decoration-none py-1">People I follow</a>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        // jQuery Document
+        $(document).ready(function() {});
+    </script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
+</html>
