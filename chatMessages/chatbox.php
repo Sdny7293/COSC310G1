435593d76cce74992f5a34a81fcbaba3834f0f6b
Sydney <sdny7293@gmail.com>
2022-07-18 18:50:37 -0700

    Layout and basic styling of message content

--- chatMessages/chatbox.php
+++ chatMessages/chatbox.php
@@ -16,27 +16,27 @@
 
 <body>
     <header>
-        <?php include '../navBar/navBar.php'?>
+        <?php include '../navBar/navBar.php' ?>
     </header>
 
-    <div class= "m-auto container-width border border-secondary">
-        <div class = "d-flex m-auto border border-primary">
+    <div class="m-auto container-width border border-secondary">
+        <div class="d-flex m-auto border border-primary vh-100">
             <div class="col-4 border col-27-5">
                 Summary of messages
             </div>
-            <div class="col border col-margin">
+            <div class="col border col-margin position-relative">
                 <!-- Message header -->
                 <div class="row border m-auto" id="menu">
                     <div class="col text-start border">
-                        <div id = "msg-sender-profile-name" class="row helv-bold pt-1">
-                            <p class = "mb-0 fs-7">Donald Smith</p>
+                        <div id="msg-sender-profile-name" class="row helv-bold pt-1">
+                            <p class="mb-0 fs-7">Donald Smith</p>
                         </div>
                         <!-- If user is active then bio section shows active -->
                         <!-- <div id = "msg-user-active">
                             <p>Active</p>
                         </div> -->
                         <div class="row helv-reg text-muted">
-                            <p class = "fs-8 overflow-hidden mb-0 height">Mechanical engineering student with a minor in computer science</p>
+                            <p class="fs-8 overflow-hidden mb-0 height">Mechanical engineering student with a minor in computer science</p>
                         </div>
                     </div>
                     <div class="col-2 border align-items-center d-flex justify-content-end">
@@ -46,32 +46,33 @@
                 <!-- Chat history -->
                 <div id="chatbox">
                     <!-- Proifile header -->
-                    <div id = "message-sender-profile">
-                        <img id = "header-profile-pic" src = "../chatMessages/images/profilepic.jpg" alt = "profile picture"/>
-                        <p id = "header-name" class = "helv-bold">Donald Smith<span id = "connection" class = "text-muted helv-reg fs-7"> • 1st</span></p>
-                        <p id = "header-bio" class = "fs-7">Student at UCLA</p>
+                    <div id="message-sender-profile">
+                        <img id="header-profile-pic" src="../chatMessages/images/profilepic.jpg" alt="profile picture" />
+                        <p id="header-name" class="helv-bold">Donald Smith<span id="connection" class="text-muted helv-reg fs-7"> • 1st</span></p>
+                        <p id="header-bio" class="fs-7 mb-0">Student at UCLA</p>
                     </div>
                     <!-- Chat content -->
-                    <div id = "chat-messages">
-                        <div id = "message-sent-date">
-                            <span id = "msg-date" class = "fs-8 helv-bold">TODAY</span>
+                    <div id="chat-messages">
+                        <div id="message-sent-date">
+                            <span id="msg-date" class="fs-9 helv-bold text-muted">TODAY</span>
                         </div>
-                        <div id = "message-sender" class = "row">
-                            <div class = "col-2">Profile picture</div>
-                            <div class = "col">
-                                <p>Sender name and time sent</p>
+                        <div id="message-sender" class="row">
+                            <div class="col-1 me-3 ps-3 pe-0">
+                                <img id="msg-profile-pic" src="../chatMessages/images/profilepic.jpg" alt="profile picture" />
+                            </div>
+                            <div class="col pt-1">
+                                <p class="helv-bold fs-7 mb-0 ms-2">Donald Smith<span class="text-muted fs-8 helv-reg"> • 10:02 AM</span></p>
                                 <!-- message content -->
-                                <p>Message content</p>
+                                <p class="helv-reg fs-7 ms-2">Message content</p>
                             </div>
                         </div>
                     </div>
                 </div>
-
                 <!-- Textbox to send message -->
-                <form name="message" action="">
-                    <input name="usermsg" type="text" id="usermsg" placeholder = "Write a message..."/>
-                    <input name="submitmsg" type="submit" id="submitmsg" value="Send" />
-                </form>
+                <div class = "position-absolute bottom-0 start-50 translate-middle-x w-100">
+                        <textarea id = "msg-textarea" class="p-2 mb-2 form-control" name="usermsg" type="text" id="usermsg" placeholder="Write a message..." rows="3"></textarea>
+                        <button id="send" type="button" class="btn btn-primary float-end rounded-5 me-2">Send</button>
+                </div>
             </div>
             <div class="col-27-5 border">
                 Right Tab
