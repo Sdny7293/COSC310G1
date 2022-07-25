bbee3f662d7120b81fca225ecef60b1bce922d53
Sydney <sdny7293@gmail.com>
2022-07-25 15:12:31 -0700

    Merged G1-31 to dev

--- client/chatbox.php
+++ client/chatbox.php
@@ -7,7 +7,7 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <meta name="description" contents="chatbox main screen" />
     <title>Chat box</title>
-    <link rel="stylesheet" href="css/chatbox.css" />
+    <link rel="stylesheet" href="../css/chatbox.css" />
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <!-- Font -->
