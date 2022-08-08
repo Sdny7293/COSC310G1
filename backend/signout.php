69d6cedb18517173b05f42bc73257000555acab0
MikhailTao <71902705+MikhailTao@users.noreply.github.com>
2022-08-08 14:07:40 -0700

    Delete signout.php

--- backend/signout.php
+++
@@ -1,9 +0,0 @@
-<?php
-foreach($_COOKIE as $key=>$val){
-    setcookie($key,"",time()-3600);
-}
-exit("<script>
-        alert('Sign Out Successfully');
-        location.href='login.html';
-        </script>");
-?>
