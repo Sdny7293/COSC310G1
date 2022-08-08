ce858ac3db648824e44bbe4f79c398bd9c4c81f8
Sydney <sdny7293@gmail.com>
2022-08-08 08:44:36 -0700

    Updated receiver names

--- client/chatbox.php
+++ client/chatbox.php
@@ -11,40 +11,40 @@
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <!-- Font -->
-    <link rel="stylesheet" href="/COSC310G1/chatMessages/css/theme.css" />
+    <link rel="stylesheet" href="../css/theme.css" />
 </head>
 
-<body ng-controller="GetRequestController" class="mx-auto d-flex flex-column vh-100">
+<body ng-controller="GetRequestController" ng-init="getAllMessages()" class="mx-auto d-flex flex-column vh-100">
     <div class="d-flex h-10 w-100 mx-auto">
         <?php include '../navBar/navBar.php' ?>
     </div>
-    <div class="mx-auto d-flex flex-row border border-warning h-90 container-width">
+    <div class="mx-auto d-flex flex-row border border-warning h-90 container-width"> 
         <!-- Message summary column -->
         <div id="msg-summary-tab" class="col-27-5 helv-bold d-flex flex-column">
             <!-- Messages header -->
             <div class="w-100 border-bottom fs-5-5 ps-3 pt-2 pb-2">
-                Messages
+                Messaging
             </div>
             <!-- Search message bar -->
             <div class="m-2">
-                <input id="search-msg-bar" class="helv-reg w-100" type="text" placeholder="Search Messages">
+                <input id="search-msg-bar" class="helv-reg w-100 p-1" type="text" placeholder="Search Messages">
             </div>
             <!-- individual message summaries -->
-            <div class="overflow-auto">
-                <div ng-repeat="message in messages" class="d-flex flex-row">
+            <div class = "overflow-auto">
+                <div ng-repeat="message in messages" class="d-flex flex-row border-bottom pt-2 ps-1 pb-2">
                     <div id="msg-summary-profpic">
                         <img id="msg-summary-profile-pic" src="../images/profilepic.jpg" alt="profile picture" />
                     </div>
-                    <div id="msg-summary-info" class="d-flex flex-column flex-grow-1 border-bottom">
-                        <div class="helv-reg fs-5-5">
+                    <div id="msg-summary-info" class="d-flex flex-column flex-grow-1">
+                        <div class="helv-reg fs-5-5 pt-2">
                             <span class="ps-0 pe-0">{{message.sender}}</span>
                         </div>
                         <div class="helv-reg fs-6">
-                            Donald: Hello
+                        {{message.sender}}: {{message.message}}
                         </div>
                     </div>
                     <div>
-                        <p class="fs-6">10:56 PM</p>
+                        <p class="fs-7 helv-reg pt-3 pe-2">{{message.date_sent}}</p>
                     </div>
                 </div>
             </div>
@@ -55,7 +55,7 @@
             <div class="border mx-auto d-flex flex-row w-100" id="menu">
                 <div class="border d-flex flex-column col-11">
                     <div id="msg-sender-profile-name" class="helv-bold pt-1">
-                        <p class="mb-0 fs-7">Name here</p>
+                        <p class="mb-0 fs-7">{{receiver}}</p>
                     </div>
                     <!-- If user is active then bio section shows active -->
                     <!-- <div id = "msg-user-active">
@@ -70,11 +70,11 @@
                 </div>
             </div>
             <!-- Chat history -->
-            <div id="chatbox" ng-init='getAllMessages()' class="d-flex flex-column overflow-auto border border-danger">
+            <div id="chatbox" class="d-flex flex-column overflow-auto border border-danger">
                 <!-- Profile header -->
-                <div id="message-sender-profile" class="border d-flex flex-column">
+                <div id="message-sender-profile" class="d-flex flex-column">
                     <img id="header-profile-pic" src="../images/profilepic.jpg" alt="profile picture" />
-                    <p id="header-name" class="helv-bold">Name here<span id="connection" class="text-muted helv-reg fs-7"> • 1st</span></p>
+                    <p id="header-name" class="helv-bold">{{receiver}}<span id="connection" class="text-muted helv-reg fs-7"> • 1st</span></p>
                     <p id="header-bio" class="fs-7 mb-0">Student at UCLA</p>
                 </div>
                 <!-- Chat content -->
@@ -88,7 +88,7 @@
                             <img id="msg-profile-pic" src="../images/profilepic.jpg" alt="profile picture" />
                         </div>
                         <!-- message content -->
-                        <div class="d-flex flex-column">
+                        <div class = "d-flex flex-column">
                             <div class="pt-1">
                                 <p class="helv-bold fs-7 mb-0 ms-2">{{message.sender}}<span class="text-muted fs-8 helv-reg"> • {{message.time_sent}}</span></p>
                                 <p class="helv-reg fs-7 ms-2">{{message.message}}</p>
@@ -99,8 +99,8 @@
             </div>
             <!-- Textbox to send message -->
             <div class="w-100">
-                <textarea id="msg-textarea" ng-model="msg" class="p-2 mb-2 form-control" name="usermsg" type="text" id="usermsg" placeholder="Write a message..." rows="3"></textarea>
-                <button id="send" ng-click='sendMsg()' type="button" class="btn btn-primary float-end rounded-5 me-2">Send</button>
+                    <textarea id="msg-textarea" ng-model="msg" class="p-2 mb-2 form-control" name="usermsg" type="text" id="usermsg" placeholder="Write a message..." rows="3"></textarea>
+                    <button id="send" ng-click='sendMsg()' type="button" class="btn btn-primary float-end rounded-5 me-2">Send</button>
             </div>
         </div>
         <!-- Right tab column -->
@@ -115,6 +115,7 @@
         function GetRequestController($scope, $http) {
             var messageArr = [];
             $scope.messages = messageArr;
+
             $scope.getAllMessages = function() {
                 $http.get('../backend/processRequest.php', {
                         params: {
@@ -128,6 +129,8 @@
                             var message = data[i].content;
                             var date_sent = data[i].date_sent;
                             var time_sent = data[i].time_sent;
+
+                            $scope.receiver = data[i].receiver_id;
                             $scope.messages.push({
                                 sender: sender,
                                 receiver: receiver,
@@ -143,12 +146,14 @@
                         alert(config)
                     });
             }
+
             $scope.sendMsg = function() {
                 $scope.sender = '<?php echo  "sender"; ?>'
                 $scope.receiver = '<?php echo  "receiver"; ?>'
                 var today = new Date();
                 var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
                 var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+                // var dateTime = date + " " + time;
 
                 //create a http connection to send message to processRequest.php
                 $http.get('../backend/processRequest.php', { ///EchoServlet/echoserve will be my php file
@@ -161,6 +166,7 @@
                             receiver: $scope.receiver,
                             date: date,
                             time: time
+                            // dateTime: dateTime
                         }
                     })
                     .success(function(data, status, headers, config) {
